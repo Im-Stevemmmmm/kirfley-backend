5d2ed8fba1130acabd0150ca876381112a7cac1d
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-19 20:45:07 -0800

    Added connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from "./resources/users/users.module";
-import { AppContext } from "./utils/app-context";
+import { ServerContext } from "./utils/app-context";
 
 @Module({
   imports: [
+    MongooseModule.forRoot(process.env.CONNECTION_STRING),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
-      context: ({ req, res }): AppContext => ({
+      context: ({ req, res }): ServerContext => ({
         req,
         res,
       }),
